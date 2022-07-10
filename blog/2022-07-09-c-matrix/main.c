65c4cbbd66052f4eed36716ae145ec3561ce84b8
MURATA Mitsuharu <hikari.photon+dev@gmail.com>
2022-07-10 18:18:04 +0900

    update c matrix

--- blog/2022-07-09-c-matrix/main.c
+++ blog/2022-07-09-c-matrix/main.c
@@ -4,6 +4,8 @@
 int main(int argc, char* argv[]){
     int i, j;
     int n_rows, n_cols;
+    int **matrix;
+    int *ary;
 
     if(argc != 3){
         fprintf(stderr, "エラー: 引数が正しく入力されていません。\n");
@@ -12,20 +14,18 @@ int main(int argc, char* argv[]){
     n_rows = (int)strtol(argv[1], NULL, 10);
     n_cols = (int)strtol(argv[2], NULL, 10);
 
-    int **matrix = (int**)malloc(sizeof(int*) * n_rows);
-
+    matrix = (int**)malloc(sizeof(int*) * n_rows);
     if(matrix == NULL){
         fprintf(stderr, "エラー: メモリが確保出来ません。\n");
         return EXIT_FAILURE;
     }
-
-    for(i = 0; i < n_rows; i++){
-        matrix[i] = (int*)malloc(sizeof(int) * n_cols);
-        if(matrix[i] == NULL){
-            fprintf(stderr, "エラー: メモリが確保出来ません。\n");
-            exit(EXIT_FAILURE);
-        }
+    ary = malloc(sizeof(int) * n_cols * n_rows);
+    if(matrix == NULL){
+        fprintf(stderr, "エラー: メモリが確保出来ません。\n");
+        return EXIT_FAILURE;
     }
+    for(i = 0; i < n_rows; i++)
+        matrix[i] = &ary[n_cols * i];
 
     /* 行列に値を入れていく */
     for(i = 0; i < n_rows; i++)
@@ -39,9 +39,7 @@ int main(int argc, char* argv[]){
         putchar('\n');
     }
 
-    for(i = 0; i < n_rows; i++){
-        free(matrix[i]);
-    }
+    free(ary);
     free(matrix);
 
     return 0;
